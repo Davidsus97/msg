bd55fad468bc882b4f9168d4497410bbabadcbab
Davidsus97 <davidsus97@gmail.com>
2023-09-04 22:45:06 -0400

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7uvtCPrfR_1rpEUqAPjKdfKCfZuFoblA",
+  authDomain: "chat-5afbf.firebaseapp.com",
+  projectId: "chat-5afbf",
+  storageBucket: "chat-5afbf.appspot.com",
+  messagingSenderId: "62634673045",
+  appId: "1:62634673045:web:fea6656d123d3fd1d7ac9c",
+  measurementId: "G-XJH5168Q6G"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          
     </>
   )
 }
